6de16c8fd2a596e6ba3992b38b050f31d7965ff8
Boris Tarelkin <tarelkinba@gmail.com>
2020-09-24 11:39:54 +0500

    Исправил тесты, чтобы падало на сравнении по ==

--- task04/src/com/example/task04/Tests.java
+++ task04/src/com/example/task04/Tests.java
@@ -12,31 +12,31 @@ public class Tests {
 
     @Test
     public void testPlus() throws Exception {
-        check(2, 2, "+", 4f);
-        check(-12, 2, "+", -10f);
-        check(-100, -99, "+", -199f);
+        check(2, 2, new String("+"), 4f);
+        check(-12, 2, new String("+"), -10f);
+        check(-100, -99, new String("+"), -199f);
     }
 
     @Test
     public void testMinus() throws Exception {
-        check(1234, 234, "-", 1000f);
-        check(-55, 5, "-", -60f);
-        check(624, -8, "-", 632f);
+        check(1234, 234, new String("-"), 1000f);
+        check(-55, 5, new String("-"), -60f);
+        check(624, -8, new String("-"), 632f);
     }
 
     @Test
     public void testDiv() throws Exception {
-        check(-25, 5, "/", -5f);
-        check(5, 2, "/", 2.5f);
-        check(15, -3, "/", -5f);
+        check(-25, 5, new String("/"), -5f);
+        check(5, 2, new String("/"), 2.5f);
+        check(15, -3, new String("/"), -5f);
     }
 
     @Test
     public void testMul() throws Exception {
-        check(-10, -10, "*", 100f);
-        check(10, 10, "*", 100f);
-        check(-10, 10, "*", -100f);
-        check(10, -10, "*", -100f);
+        check(-10, -10, new String("*"), 100f);
+        check(10, 10, new String("*"), 100f);
+        check(-10, 10, new String("*"), -100f);
+        check(10, -10, new String("*"), -100f);
     }
 
 }
